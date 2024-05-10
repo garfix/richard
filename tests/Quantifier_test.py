920695e21986a51bc303716c804c5fd3283fe61e
Patrick van Bergen <patrick.vanbergen@gmail.com>
2024-05-10 08:02:27 +0200

    save

--- tests/Quantifier_test.py
+++ tests/Quantifier_test.py
@@ -32,27 +32,30 @@ from lib.store.MemoryDb import MemoryDb
 def find(quant: tuple[callable, callable], vp: callable) -> list:
     print('find', vp)
     qp, nbar = quant
-    print('call nbar', nbar)
     elements = nbar()
     range_count = len(elements)
 
-    print('call vp', vp)
+    print('s-------')
     result = []
     for element in elements:
         for e2 in vp(element):
             result.append(e2)
+    result = list(set(result))
     result_count = len(result)
-    print(result_count, range_count)
+    print('e-------')
+    print(result_count, result)
     if qp(result_count, range_count):
+        print('correct number')
         return result
     else:
+        print('wrong number')
         return []
 
 db = MemoryDb()
 db.assert_record(Record('has_child', {'parent': 'mary', 'child': 'lucy'}))
 db.assert_record(Record('has_child', {'parent': 'mary', 'child': 'bonny'}))
 db.assert_record(Record('has_child', {'parent': 'barbara', 'child': 'john'}))
-db.assert_record(Record('has_child', {'parent': 'barbara', 'child': 'check'}))
+db.assert_record(Record('has_child', {'parent': 'barbara', 'child': 'chuck'}))
 db.assert_record(Record('has_child', {'parent': 'william', 'child': 'oswald'}))
 db.assert_record(Record('has_child', {'parent': 'william', 'child': 'bertrand'}))
 
@@ -61,10 +64,10 @@ db.assert_record(Record('has_child', {'parent': 'william', 'child': 'bertrand'})
 def s(np, vp_no_sub_a): 
     return lambda: find(np(), vp_no_sub_a)
 
-def vp_no_sub(aux, qp, child):
+def has_child(aux, qp, parent):
     return lambda sub: find(
-        (qp, child),
-        lambda sub2: [r.values['child'] for r in db.match(Record('has_child', {'parent': sub}))])
+        (qp, parent),
+        lambda obj: list(set([r.values['child'] for r in db.match(Record('has_child', {'parent': sub, 'child': obj}))])))
 
 class TestQuantifier(unittest.TestCase):
    
@@ -73,7 +76,7 @@ class TestQuantifier(unittest.TestCase):
 
         grammar = [
             { "syn": "s -> np vp_no_sub", "sem": s },
-            { "syn": "vp_no_sub -> aux qp child", "sem": vp_no_sub },
+            { "syn": "vp_no_sub -> aux qp child", "sem": has_child },
             { "syn": "np -> qp nbar", "sem": lambda qp, nbar: lambda: (qp, nbar) },
             { "syn": "qp -> quantifier", "sem": lambda quantifier: lambda result, range: quantifier(result, range) },
             { "syn": "quantifier -> det", "sem": lambda det: lambda result, range: det(result, range) },
@@ -134,7 +137,7 @@ class TestQuantifier(unittest.TestCase):
         result = request.get_current_product(parser)
         # print(result)
 
-        result = request.get_current_product(executor)
+        result = request.get_alternative_products(executor)
         print("result: ", result)
         # self.assertEqual(len(result), 3)
 
