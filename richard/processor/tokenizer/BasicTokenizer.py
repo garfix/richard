47fe397c08caa9f823dca8fc4e8be0fff68158b2
Patrick van Bergen <patrick.vanbergen@gmail.com>
2024-05-13 20:18:22 +0200

    extend basic tokenizer

--- richard/processor/tokenizer/BasicTokenizer.py
+++ richard/processor/tokenizer/BasicTokenizer.py
@@ -1,11 +1,20 @@
+import re
 from richard.entity.SentenceRequest import SentenceRequest
 from richard.interface.SomeTokenizer import SomeTokenizer
 
 
 class BasicTokenizer(SomeTokenizer):
 
+    BASIC_TOKEN_RE = "([a-zA-Z_0-9]+|[^\\s])"
+    
+    token_expression: re.Pattern
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.token_expression = re.compile(self.BASIC_TOKEN_RE)
+
     def process(self, request: SentenceRequest):
-        tokens = request.text.split(" ")
+        tokens = self.token_expression.findall(request.text)
         return [tokens]
 
 
