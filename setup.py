74645afef9e0f3ed4e0803cd28158fb2bacd76e7
Patrick van Bergen <patrick.vanbergen@gmail.com>
2024-05-19 10:33:09 +0200

    long desc

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     name='richard',
     version='0.2.0',    
     description='A Natural Language Understanding and Execution library',
+    long_description='This library turns a question, statement or command from a ordinary human language text directly into Python executable code. It is completely rule-based and you as a developer define the code that is to be executed. The executable code can be any Python code, and there is specially suited for database access.',
     url='https://github.com/garfix/richard',
     author='Patrick van Bergen',
     author_email='patrick.vanbergen@gmail.com',
