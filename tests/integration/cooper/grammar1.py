ed6c7eef53fc1f063aac426309f977fd321dba74
Patrick van Bergen <patrick.vanbergen@gmail.com>
2024-10-29 08:16:28 +0100

    that is

--- tests/integration/cooper/grammar1.py
+++ tests/integration/cooper/grammar1.py
@@ -14,60 +14,59 @@ def get_grammar1():
 
         # X is a Y
         {
-            "syn": "s(E1) -> noun(E1, T1) is() a() np(E1, T1)",
+            "syn": "s() -> noun(E1, T1) is() a() np(E1, T1)",
             "sem": lambda noun, is1, a, np: [('=', T1, 'true')] + noun + [('store', np)],
-            "inf": [("format", "canned"), ("format_canned", "OK")],
-        },
-        # X is a Y that is Z
-        {
-            "syn": "s(E1) -> noun(E1, T1) is() a() np(E1, T1) 'that' is() np(E1, T1)",
-            "sem": lambda noun, is1, a, np1, is2, np2: [('=', T1, 'true')] + noun + [('store', np1 + np2)],
             "inf": [("format", "canned"), ("format_canned", "OK")],
         },
         # no X is a Y
         {
-            "syn": "s(E1) -> 'no' noun(E1, T1) is() a() np(E1, T2)",
+            "syn": "s() -> 'no' noun(E1, T1) is() a() np(E1, T2)",
             "sem": lambda noun, is1, a, np: [('=', T1, 'true'), ('=', T2, 'false'), ('learn_rule', np[0], noun)],
             "inf": [("format", "canned"), ("format_canned", "OK")],
         },
-        # X is not a Y
+        # a X is not a Y
         {
-            "syn": "s(E1) -> a() noun(E1, T1) is() 'not' a() np(E1, T2)",
+            "syn": "s() -> a() noun(E1, T1) is() 'not' a() np(E1, T2)",
             "sem": lambda a1, noun, is1, a2, np: [('=', T1, 'true'), ('=', T2, 'false'), ('learn_rule', np[0], noun)],
             "inf": [("format", "canned"), ("format_canned", "OK")],
         },
+
+
+
+
         # noun verb
         {
-            "syn": "s(E1) -> noun(E1, T1) verb(E1)",
+            "syn": "s() -> noun(E1, T1) verb(E1)",
             "sem": lambda noun, verb: noun + [('store', verb)],
             "inf": [("format", "canned"), ("format_canned", "OK")],
         },
+        # combustable things burn
         {
-            "syn": "s(E1) -> adj(E1, T1) 'things' verb(E1)",
-            "sem": lambda adj, verb: [('=', T1, 'true'), ('=', T2, 'true'), ('learn_rule', verb[0], adj)],
+            "syn": "s() -> np(E1, T1) verb(E1)",
+            "sem": lambda np, verb: [('=', T1, 'true'), ('=', T2, 'true'), ('learn_rule', verb[0], np)],
             "inf": [("format", "canned"), ("format_canned", "OK")],
         },
         # dark-gray things are not white
         {
-            "syn": "s(E1) -> adj(E1, T1) 'things' are() 'not' adj(E1, T2)",
-            "sem": lambda adj1, are, adj2: [('=', T1, 'true'), ('=', T2, 'false'), ('learn_rule', adj2[0], adj1)],
+            "syn": "s() -> np(E1, T1) are() 'not' adj(E1, T2)",
+            "sem": lambda np, are, adj2: [('=', T1, 'true'), ('=', T2, 'false'), ('learn_rule', adj2[0], np)],
             "inf": [("format", "canned"), ("format_canned", "OK")],
         },
         # gasoline is combustable
         {
-            "syn": "s(E1) -> noun(E1, T1) 'is' adj(E1, T1)",
+            "syn": "s() -> noun(E1, T1) 'is' adj(E1, T1)",
             "sem": lambda noun, adj: [('=', T1, 'true'), ('=', T2, 'true'), ('learn_rule', adj[0], noun)],
             "inf": [("format", "canned"), ("format_canned", "OK")],
         },
         # X is Y (X is another name for Y)
         {
-            "syn": "s(E1) -> proper_noun(E1) 'is' proper_noun(E1)",
+            "syn": "s() -> proper_noun(E1) 'is' proper_noun(E1)",
             "sem": lambda proper_noun1, proper_noun2: proper_noun1 + proper_noun2,
             "inf": [("format", "canned"), ("format_canned", "OK")],
         },
         # X's are not Y's
         {
-            "syn": "s(E1) -> noun(E1, T1) are() 'not' np(E1, T2)",
+            "syn": "s() -> noun(E1, T1) are() 'not' np(E1, T2)",
             "sem": lambda noun, are, np: [
                 ('scoped', [('=', T1, 'false'), ('=', T2, 'true'), ('learn_rule', noun[0], np)]),
                 ('scoped', [('=', T1, 'true'), ('=', T2, 'false'), ('learn_rule', np[0], noun)])
@@ -76,13 +75,13 @@ def get_grammar1():
         },
         # X's are Y's
         {
-            "syn": "s(E1) -> noun(E1, T1) are() np(E1, T2)",
+            "syn": "s() -> noun(E1, T1) are() np(E1, T2)",
             "sem": lambda noun, are, np: [('=', T1, 'true'), ('=', T2, 'true'), ('learn_rule', np[0], noun)],
             "inf": [("format", "canned"), ("format_canned", "OK")],
         },
         # any thing that burns rapidly burns
         {
-            "syn": "s(E1) -> 'any' 'thing' 'that' verb(E1) verb(E1)",
+            "syn": "s() -> 'any' 'thing' 'that' verb(E1) verb(E1)",
             "sem": lambda verb1, verb2: [('=', T1, 'true'), ('=', T2, 'true'), ('learn_rule', verb2[0], verb1)],
             "inf": [("format", "canned"), ("format_canned", "OK")],
         },
@@ -91,6 +90,7 @@ def get_grammar1():
         # np
         { "syn": "np(E1, T1) -> noun(E1, T1)", "sem": lambda noun: noun },
         { "syn": "np(E1, T1) -> adj(E1, T1) np(E1, T1)", "sem": lambda adj, noun: adj + noun },
+        { "syn": "np(E1, T1) -> np(E1, T1) 'that' 'is' np(E1, T1)", "sem": lambda np1, np2: np1 + np2 },
 
         # verb
         { "syn": "verb(E1) -> 'burn'", "sem": lambda: [('burns', E1, 'true')] },
@@ -128,6 +128,7 @@ def get_grammar1():
         { "syn": "common_noun(E1, T1) -> 'elements'", "sem": lambda: [('element', E1, T1)] },
         { "syn": "common_noun(E1, T1) -> 'compound'", "sem": lambda: [('compound', E1, T1)] },
         { "syn": "common_noun(E1, T1) -> 'compounds'", "sem": lambda: [('compound', E1, T1)] },
+        { "syn": "common_noun(E1, T1) -> 'things'", "sem": lambda: [] },
 
         # proper noun
         # "magnesium"
