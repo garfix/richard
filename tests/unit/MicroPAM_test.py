67a81e9e6604ff5daf0874916baceef093159314
Patrick van Bergen <patrick.vanbergen@gmail.com>
2026-01-29 07:48:08 +0100

    plan

--- tests/unit/MicroPAM_test.py
+++ tests/unit/MicroPAM_test.py
@@ -8,6 +8,7 @@ class TestMicroPAM(unittest.TestCase):
 
         # relate themes to goals
         init_rules = [
+            # theme: to be in a fed state; goal: to not be hungry
             [
                 [['is', ['actor', '?x'], ['state', ['hunger', ['val', '?n']]]], ['pos-val', '?n']],
                 [['goal', ['planner', '?x'], ['objective', ['is', ['actor', '?x'], ['state', ['hunger', ['val', [0]]]]]]]]
@@ -16,30 +17,36 @@ class TestMicroPAM(unittest.TestCase):
 
         # relate goals to plans
         plans_for = [
+            # to possess an object, take the object
             [
                 [['goal', ['planner', '?x'], ['objective', ['poss', ['actor', '?x'], ['object', '?y']]]]],
                 [['take-plan', ['planner', '?x'], ['object', '?y']]]
             ],
+            # to know the distance to a restaurant, read a restaurant guide
             [
                 [['goal', ['planner', '?x'],
                           ['objective', ['know', ['actor', '?x'],
                                                  ['fact', ['is', ['actor', 'restaurant'], ['prox', '?z']]]]]]]
                 [['read-plan', ['planner', '?x'], ['object', '?w']], ['isa', 'restaurant-guide', '?w']]
             ],
+            # to enjoy oneself, read a book
             [
                 [['goal', ['planner', '?x'], ['objective', ['enjoyment', ['actor', '?x']]]]],
                 [['read-plan', ['planner', '?x'], ['object', '?w']], ['isa', 'book', '?w']]
 
             ],
+            # to not be hungry, go to a restaurant
             [
                 [['goal', ['planner', '?x'], ['objective', ['is', ['actor', '?x'], ['state', ['hunger', ['val', [0]]]]]]]],
                 [['do-$restaurant-plan', ['planner', '?x'], ['restaurant', '?y']]]
 
             ],
+            # to be near a location, walk there
             [
                 [['goal', ['planner', '?x'], ['objective', ['prox', ['actor', '?x'], ['location', '?y']]]]],
                 [['walk-plan', ['planner', '?x'], ['location', '?y']]]
             ],
+            # to be near a location, use a vehicle
             [
                 [['goal', ['planner', '?x'], ['objective', ['prox', ['actor', '?x'], ['location', '?y']]]]],
                 [['use-vehicle-plan', ['planner', '?x']]]
@@ -49,28 +56,40 @@ class TestMicroPAM(unittest.TestCase):
 
         # preconditions of a plan
         sub_for = [
+            # to read, possess a book
             [
                 [['read-plan', ['planner', '?x'], ['object', '?y']]],
                 [['goal', ['planner', '?x'],
                           ['objective', ['poss', ['actor', '?x'], ['object', '?y']]]], ['isa', 'book', '?y']]
             ],
+            # to be close to a location, know ...(?)
             [
-
+                [['goal', ['planner', '?x'], ['objective', ['prox', ['actor' '?x'], ['location', '?y']]]]],
+                [['goal', ['planner', '?x'],
+                          ['objective', ['know', ['actor', '?x'], ['fact', ['is', ['actor', '?y'], ['prox', '?z']]]]]]]
             ],
+            # to visit a restaurant, be close to a restaurant
             [
-
+                [['do-$restaurant-plan', ['planner', '?x'], ['restaurant', '?y']]],
+                [['goal', ['planner', '?x'],
+                          ['objective', ['prox', ['actor', '?x'], ['location', '?y']]]], ['isa', 'restaurant', '?y']]
             ],
+            # to use a vehicle, be close to a car
             [
-
+                [['use-vehicle-plan', ['planner', '?x'], ['object', '?y']]],
+                [['goal', ['planner', '?x'],
+                          ['objective', ['prox', ['actor', '?x'], ['location', '?y']]]], ['isa', 'car', '?y']]
             ],
         ]
 
         # possible instances of a plan
         instof = [
+            # to take an object, means to grasp it
             [
                 [['take-plan', ['planner', '?x'], ['object', '?y']]],
                 [['grasp', ['actor', '?x'], ['object', '?y']]]
             ],
+            # to walk to a location, means to physically transfer yourself to it
             [
                 [['walk-plan', ['planner', '?x'], ['location', '?y']]],
                 [['ptrans', ['actor', '?x'], ['object', '?x'], ['to', '?y']]]
